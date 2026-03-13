869cfd7c3c1e716ae711ef558f03003a8b4b35de
Koichi-Shigihara2 <jamablue01+github02@gmail.com>
2026-03-14 08:19:02 +0900

    Update edgar_fetcher.py

--- src/edgar_fetcher.py
+++ src/edgar_fetcher.py
@@ -1,6 +1,10 @@
-from edgar import Company
+from edgar import Company, set_identity
 
 def fetch_filings(ticker):
+    # SECのルールに従い、自分の名前とメールアドレスをセットします
+    # (例: "YourName yourname@example.com")
+    set_identity("jamablue01@gmail.com") 
+    
     company = Company(ticker)
     filings = company.get_filings(form=["10-Q","10-K"])
     return [str(filing.accession_no) for filing in filings[:8]]
