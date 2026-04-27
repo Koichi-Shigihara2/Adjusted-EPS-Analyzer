7c9f325e165d71a2d5dd65b764a797da0fe0d8af
Koichi Shigihara <jamablue01@gmail.com>
2026-04-27 20:28:12 +0900

    chore: remove temp check scripts

--- check_rd.py
+++
@@ -1,8 +0,0 @@
-import json
-with open('common/sec_data/data/MSFT/company_facts.json', encoding='utf-8') as f:
-    d = json.load(f)
-rd = d['facts']['us-gaap']['ResearchAndDevelopmentExpense']
-units = rd.get('units', {}).get('USD', [])
-annual = [x for x in units if x.get('form') == '10-K']
-for x in sorted(annual, key=lambda x: x.get('end',''))[-5:]:
-    print(x.get('end'), f"${x.get('val'):,.0f}")
